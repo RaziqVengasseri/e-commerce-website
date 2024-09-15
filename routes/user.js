3441a1cbe2bd8c9aa63bbff08d592897a7cdaa7a
Mohammed Raziq V <anuvraziq@gmail.com>
2024-09-16 00:47:30 +0530

    completed part-4

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let products = [
-    {
-      name:'I phone',
-      category:'Mobile',
-      description:'This is a good phone',
-      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRri6sgt9ndwMBJvxv4SVsFNZFUnQdJ3vaGDQ&s'
-    },
-    {
-      name:'Samsung',
-      category:'Mobile',
-      description:'This is a good phone',
-      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR6omwZVBiRn__EC28XQ3UGePxVPDNErd0lg&s'
-    },
-    {
-      name:'Vivo',
-      category:'Mobile',
-      description:'This is a good phone',
-      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5fz6exyJfKrblOuO2IRUWsFb9_QaceMfe-Q&s'
-    },
-    {
-      name:'Motorola',
-      category:'Mobile',
-      description:'This is a good phone',
-      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTRynRkRpWap7hK_UJ5FkP-tCMtBA_9sjbxA&s'
-    }
-  ]
-  res.render('index', { products,admin:false});
+
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('user/view-products',{admin:true,products})
+  })
 });
 
 module.exports = router;
