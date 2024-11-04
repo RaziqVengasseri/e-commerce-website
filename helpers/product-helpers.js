c3a00db2e38ff47f08e90fd735e716151d17a2b4
Mohammed Raziq V <anuvraziq@gmail.com>
2024-11-04 14:05:39 +0530

    Part - 7 completed

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,26 +1,64 @@
-var db=require('../config/connection')
+var db = require('../config/connection')
 var collection = require('../config/collections')
-var {ObjectId} = require ('mongodb')
-module.exports={
-  addProduct:(product,callback)=>{
+var { ObjectId } = require('mongodb')
+const { response } = require('../app')
 
-    db.get().collection('product').insertOne(product).then((data)=>{
+module.exports = {
+  addProduct: (product, callback) => {
+    db.get().collection('product').insertOne(product).then((data) => {
       console.log(data)
       callback(data.insertedId.toString())
     })
   },
-  getAllProducts:()=>{
-    return new Promise(async(resolve,reject)=>{
-      let products= await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+  
+  getAllProducts: () => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
       resolve(products)
     })
   },
-  deleteProduct:(prodId)=>{
-    return new Promise((resolve,reject)=>{
-      db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:new ObjectId(prodId)}).then((response)=>{
-        //console.log(response)
+  
+  deleteProduct: (prodId) => {
+    return new Promise((resolve, reject) => {
+      db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectId(prodId) }).then((response) => {
+        // console.log(response)
         resolve(response)
       })
     })
-  }
+  },
+  
+  getProductDetails: (proId) => {
+    return new Promise((resolve, reject) => {
+      db.get().collection(collection.PRODUCT_COLLECTION).findOne({ _id: new ObjectId(proId) })
+        .then((product) => {
+          resolve(product)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+ 
+
+updateProduct: (proId, proDetails) => {
+  return new Promise((resolve, reject) => {
+    db.get().collection(collection.PRODUCT_COLLECTION)
+      .updateOne(
+        { _id: new ObjectId(proId) }, // Use 'new' keyword here
+        {
+          $set: {
+            Name: proDetails.Name,
+            Description: proDetails.Description,
+            Price: proDetails.Price,
+            Category: proDetails.Category
+          }
+        }
+      ).then((response) => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+  });
+}
+
 }
